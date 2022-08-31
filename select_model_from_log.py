87df0a123d79ad95d2e73a8dcf7186d1b5e6bdff
dyahadila <dyhadila85@gmail.com>
2022-08-31 13:15:21 -0500

    log reading

---
+++ select_model_from_log.py
@@ -0,0 +1,134 @@
+import os
+from glob import glob
+import numpy as np
+'''
+select model from logs in the same root dir
+1. get root dir
+2. scan all log.txt in the dir
+3. select baseline and ours best on best val on each
+    - look for the summary keyword
+    - look for the val perf, 
+    - select best val perf from all files
+'''
+
+def file_to_list(file_path):
+    with open(file_path) as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+# def get_numbers(section_lines):
+
+def get_cb(cb_dict, val_avg, val_wg):
+    for cb in cb_dict:
+        dict_ = cb_dict[cb]
+        b_val_avg, b_val_wg, o_val_avg, o_val_wg = list(dict_.values())
+        if (val_avg == b_val_avg and val_wg == b_val_wg) or (val_avg == o_val_avg and val_wg == o_val_wg):
+            return cb
+
+def scan_file(lines):
+    cb_dict = {}
+    for i, line in enumerate(lines):
+        if 'HYPERPARAMS' in line:
+            params_str = line
+        elif "######" in line:
+            if "######" in lines[i+1]:
+                continue
+            cb = float(line[7:10])
+            cb_dict[cb] = {}
+            cb_dict[cb]['baseline_val_avg'] = float(lines[i+3][-5:])
+            cb_dict[cb]['baseline_val_wg'] = float(lines[i+8][-5:])
+            cb_dict[cb]['ours_val_avg'] = float(lines[i+21][-5:])
+            cb_dict[cb]['ours_val_wg'] = float(lines[i+26][-5:])
+        elif (line == 'baseline val') or (line == 'baseline test') \
+            or (line == 'weighted sample val') or (line == 'weighted sample test'):
+            avg_idx = i+1
+            wg_idx = i
+            while wg_idx < len(lines) and len(lines[wg_idx]) > 0:
+                wg_idx += 1
+            wg_idx -= 1
+            avg = float(lines[avg_idx][-5:])
+            wg = float(lines[wg_idx][-5:])
+            if line == 'baseline val':
+                baseline_val_avg = avg
+                baseline_val_wg = wg
+            elif line == 'baseline test':
+                baseline_test_avg = avg
+                baseline_test_wg = wg
+            elif line == 'weighted sample val':
+                ours_val_avg = avg
+                ours_val_wg = wg
+            elif line == 'weighted sample test':
+                ours_test_avg = avg
+                ours_test_wg = wg
+            i += wg_idx
+            continue
+    
+    baseline_cb = get_cb(cb_dict, baseline_val_avg, baseline_val_wg)
+    ours_cb = get_cb(cb_dict, ours_val_avg, ours_val_wg)
+
+    return baseline_val_avg, baseline_val_wg, baseline_test_avg, baseline_test_wg, \
+            ours_val_avg, ours_val_wg, ours_test_avg, ours_test_wg,\
+            baseline_cb,  ours_cb, params_str
+
+def get_best_perf(val_list, test_list):
+    best_val_idx = np.argmax(np.array(val_list))
+    best_val_perf = val_list[best_val_idx]
+    best_test_perf = test_list[best_val_idx]
+    best_val_perf, best_test_perf, best_val_idx
+
+
+if __name__ == '__main__':
+    dataset_name = 'camelyon17'
+    log_dir = f'log/spurious_exp/{dataset_name}/TUNE_PARAMS_SPURIOUS'
+    tune_by = 'acc_avg'
+    
+    log_files =  [y for x in os.walk(log_dir) for y in glob(os.path.join(x[0], '*.txt'))]
+
+    baseline_val_avg = []
+    baseline_val_wg = []
+    baseline_test_avg = []
+    baseline_test_wg = []
+    ours_val_avg = []
+    ours_val_wg = []
+    ours_test_avg = []
+    ours_test_wg = []
+    params_str = []
+    baseline_cb = []
+    ours_cb = []
+
+    for file_ in log_files:
+        lines = file_to_list(file_)
+        if ('baseline val' not in lines) and ('baseline test' not in lines) and \
+            ('weighted sample val' not in lines) and ('weighted sample test' not in lines):
+            continue
+        b_val_avg, b_val_wg, b_test_avg, b_test_wg, \
+        o_val_avg, o_val_wg, o_test_avg, o_test_wg, \
+        b_cb, o_cb, param_str_ = scan_file(lines)
+
+        baseline_val_avg.append(b_val_avg)
+        baseline_val_wg.append(b_val_wg)
+        baseline_test_avg.append(b_test_avg)
+        baseline_test_wg.append(b_test_wg)
+        ours_val_avg.append(o_val_avg)
+        ours_val_wg.append(ours_val_wg)
+        ours_test_avg.append(o_test_avg)
+        ours_test_wg.append(o_test_wg)
+        baseline_cb.append(b_cb)
+        ours_cb.append(o_cb)
+        params_str.append(param_str_)
+
+    if tune_by == 'acc_avg':
+        best_baseline_val, best_baseline_test, best_baseline_idx = get_best_perf(baseline_val_avg, baseline_test_avg)
+        best_baseline_cb = baseline_cb[best_baseline_idx]
+
+        best_ours_val, best_ours_test, best_ours_idx = get_best_perf(ours_val_avg, ours_test_avg)
+        best_ours_cb = ours_cb[best_ours_idx]
+    elif tune_by == 'acc_wg':
+        best_baseline_val, best_baseline_test, best_baseline_idx = get_best_perf(baseline_val_wg, baseline_test_wg)
+        best_baseline_cb = baseline_cb[best_baseline_idx]
+
+        best_ours_val, best_ours_test, best_ours_idx = get_best_perf(ours_val_wg, ours_test_wg)
+        best_ours_cb = ours_cb[best_ours_idx]
+
+
+    
